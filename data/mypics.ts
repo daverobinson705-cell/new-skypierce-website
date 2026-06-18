1d1a47d98cddd02d008c4b14157cee8e200b0db1
daverobinson705-cell <daverobinson705@gmail.com>
2026-06-18 21:48:21 +0100

    I changed the pictures extension file from png to PNG

--- data/mypics.ts
+++ data/mypics.ts
@@ -1,40 +1,40 @@
 const myPics = [
     {
-        "src": "/all-images/mypics/IMG_5047.png",
+        "src": "/all-images/mypics/IMG_5047.PNG",
         "alt": "Sky Pierce",
     },
     {
-        "src": "/all-images/mypics/IMG_5121.png",
-        "alt": "Sky Pierce",
-    },
-
-    {
-        "src": "/all-images/mypics/IMG_5123.png",
+        "src": "/all-images/mypics/IMG_5121.PNG",
         "alt": "Sky Pierce",
     },
 
     {
-        "src": "/all-images/mypics/IMG_5124.png",
+        "src": "/all-images/mypics/IMG_5123.PNG",
         "alt": "Sky Pierce",
     },
 
     {
-        "src": "/all-images/mypics/IMG_5125.png",
+        "src": "/all-images/mypics/IMG_5124.PNG",
         "alt": "Sky Pierce",
     },
 
     {
-        "src": "/all-images/mypics/IMG_5126.png",
+        "src": "/all-images/mypics/IMG_5125.PNG",
         "alt": "Sky Pierce",
     },
 
     {
-        "src": "/all-images/mypics/IMG_5127.png",
+        "src": "/all-images/mypics/IMG_5126.PNG",
         "alt": "Sky Pierce",
     },
 
     {
-        "src": "/all-images/mypics/IMG_5128.png",
+        "src": "/all-images/mypics/IMG_5127.PNG",
+        "alt": "Sky Pierce",
+    },
+
+    {
+        "src": "/all-images/mypics/IMG_5128.PNG",
         "alt": "Sky Pierce",
     },
 
